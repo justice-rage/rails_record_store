80c9fe8170dab39561cd58f9d0b890ee85560f58
justicepelteir <justice.douglas@hey.com>
2022-05-22 15:21:55 -0700

    create song table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_22_190929) do
+ActiveRecord::Schema.define(version: 2022_05_22_221606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2022_05_22_190929) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "genre"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.string "lyrics"
+    t.integer "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
