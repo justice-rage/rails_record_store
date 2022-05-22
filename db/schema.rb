417e3c67e1876633a0bd51b56c9aba117a477ed6
justicepelteir <justice.douglas@hey.com>
2022-05-22 12:15:42 -0700

    add genreto albums table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_22_185218) do
+ActiveRecord::Schema.define(version: 2022_05_22_190929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_05_22_185218) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "genre"
   end
 
 end
