52d51a6c8bcebbd5bbf343f1ec6d35d2ef09785a
justicepelteir <justice.douglas@hey.com>
2022-05-23 20:25:54 -0700

    add create and private controller

--- app/controllers/songs_contoller.rb
+++ app/controllers/songs_contoller.rb
@@ -4,4 +4,19 @@ class SongsController < ApplicationController
       @song = @album.songs.new
       render :new
     end
+
+    def create
+        @album = Album.find(params[:album_id])
+        @song = @album.songs.new(song_params)
+        if @song.save
+          redirect_to album_path(@album)
+        else
+          render :new
+        end
+      end
+        
+    private
+        def song_params
+          params.require(:song).permit(:name, :lyrics)
+        end
 end
