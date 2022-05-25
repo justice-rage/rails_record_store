5e997fa5ad5fcbcf6f90d95c615893305f367956
justicepelteir <justice.douglas@hey.com>
2022-05-24 20:22:14 -0700

    add titleizes name test

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -6,4 +6,9 @@ describe Album do
   it { should validate_presence_of :name }
 
   it { should validate_length_of(:name).is_at_most(100) }
+
+  it("titelizes the name of an album") do 
+    album = Album.create({name: "giant steps", genre: "jazz"})
+    expect(album.name()).to(eq("Giant Steps"))
+  end
 end
